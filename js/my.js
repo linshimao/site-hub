75d67b477f718bab718a59942c49af64f01c9f72
linshimao <931520261@qq.com>
2016-11-05 18:58:31 +0800

    风车动画时间间隔修改为一秒

--- js/my.js
+++ js/my.js
@@ -15,57 +15,62 @@ changeBgImg();
 */
 //切换壁纸(并且将生成的随机数存入cookie)
 $(function () {
-  if ($.cookie('bgUrl')) {
-    $("#wrap").css("backgroundImage", "url(http://img.infinitynewtab.com/wallpaper/" + $.cookie("bgUrl").toString().split("bgUrl=").join('') + ".jpg");
-  }
-  $(".btn-img:eq(0)").click(function () {
-    randomNumber = Math.ceil((Math.random() * 4049)) + 1;
-    $.cookie('bgUrl', randomNumber, {
-      expires: 365
-    });
-    $("#wrap").css("backgroundImage", "url(http://img.infinitynewtab.com/wallpaper/" + $.cookie("bgUrl").toString().split("bgUrl=").join('') + ".jpg");
-  });
+	if ($.cookie('bgUrl')) {
+		$("#wrap").css("backgroundImage", "url(http://img.infinitynewtab.com/wallpaper/" + $.cookie("bgUrl").toString().split("bgUrl=").join('') + ".jpg");
+	}
+	$(".btn-img:eq(0)").click(function () {
+		$("#changeBgImg .btn-img").addClass("rotate");
+		setTimeout(function () {
+			$("#changeBgImg .btn-img").removeClass("rotate");
+		}, 1000);
+		var randomNumber = Math.ceil((Math.random() * 4049)) + 1;
+		$.cookie('bgUrl', randomNumber, {
+			expires: 365
+		});
+		$("#wrap").css("backgroundImage", "url(http://img.infinitynewtab.com/wallpaper/" + $.cookie("bgUrl").toString().split("bgUrl=").join('') + ".jpg");
+	});
 });
+//判断图片是否加载完成
 
 
 //切换nav > ul > li > a底部边框
 function changeALine() {
-  var OUl = document.getElementById('OUl');
-  var OA = OUl.getElementsByTagName('a');
-  for (var i = 0; i < OA.length; i++) {
-    OA[i].onclick = function () {
-      for (var i = 0; i < OA.length; i++) {
-        OA[i].removeAttribute("class");
-      }
-      this.setAttribute("class", "a-active");
-    }
-  }
+	var OUl = document.getElementById('OUl');
+	var OA = OUl.getElementsByTagName('a');
+	for (var i = 0; i < OA.length; i++) {
+		OA[i].onclick = function () {
+			for (var i = 0; i < OA.length; i++) {
+				OA[i].removeAttribute("class");
+			}
+			this.setAttribute("class", "a-active");
+		}
+	}
 }
 changeALine();
 
 //控制输入框搜索按钮的显示和隐藏
 function searchBtnToggle() {
-  var searchInput1 = document.getElementsByTagName("input")[0];
-  var searchInput2 = document.getElementsByTagName("input")[1];
-  searchInput1.onclick = function () {
-    searchInput2.style.backgroundImage = "url(img/search_icon.png)";
-    searchInput2.style.backgroundRepeat = "no-repeat";
-    searchInput2.style.backgroundPosition = "center";
-    searchInput2.style.backgroundSize = "50%";
-    searchInput2.style.backgroundColor = "#41f084";
-  }
-  searchInput1.onblur = function () {
-    searchInput2.style.backgroundColor = "#fff";
-  }
-  searchInput2.onmouseover = function () {
-    this.style.backgroundImage = "url(img/search_icon.png)";
-    this.style.backgroundRepeat = "no-repeat";
-    this.style.backgroundPosition = "center";
-    this.style.backgroundSize = "50%";
-    this.style.backgroundColor = "#2ECC71";
-  }
-  searchInput2.onmouseout = function () {
-    this.style.backgroundColor = "#fff";
-  }
+	var searchInput1 = document.getElementsByTagName("input")[0];
+	var searchInput2 = document.getElementsByTagName("input")[1];
+	searchInput1.onclick = function () {
+		searchInput2.style.backgroundImage = "url(img/search_icon.png)";
+		searchInput2.style.backgroundRepeat = "no-repeat";
+		searchInput2.style.backgroundPosition = "center";
+		searchInput2.style.backgroundSize = "50%";
+		searchInput2.style.backgroundColor = "#41f084";
+	}
+	searchInput1.onblur = function () {
+		searchInput2.style.backgroundColor = "#fff";
+	}
+	searchInput2.onmouseover = function () {
+		this.style.backgroundImage = "url(img/search_icon.png)";
+		this.style.backgroundRepeat = "no-repeat";
+		this.style.backgroundPosition = "center";
+		this.style.backgroundSize = "50%";
+		this.style.backgroundColor = "#2ECC71";
+	}
+	searchInput2.onmouseout = function () {
+		this.style.backgroundColor = "#fff";
+	}
 }
 searchBtnToggle();
